efe624c67c5d0cf4c8824bc5cced4c41020e2b0b
sumit160803 <smishra.msumit@gmail.com>
2024-07-19 00:58:42 +0530

    final commit

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
 
 
 //GET /users
+app.get("/users",(req,res)=>{
+    res.json(users);
+})
 
+app.get("/users/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=>user.id === id);
+    return res.json(user);
+})
+
+// other methods like PATCH POST and DELETE will be done baad mei wooohoooo
 
 app.listen(PORT,()=>console.log(`starting the server at ${PORT}`));
